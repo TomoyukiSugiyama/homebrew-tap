44977989645ba8f509d633b4ffab942e40599f85
GitHub <noreply@github.com>
2025-10-23 07:59:25 +0000

    chore: bump dotfiles to 0.2.0

--- Formula/dotfiles.rb
+++ Formula/dotfiles.rb
@@ -1,9 +1,9 @@
 class Dotfiles < Formula
   desc "Terminal UI and CLI for dotfiles management"
   homepage "https://github.com/TomoyukiSugiyama/dotfiles"
-  url "https://github.com/TomoyukiSugiyama/dotfiles/releases/download/v0.1.0/dotfiles-0.1.0-universal-apple-darwin.tar.gz"
-  sha256 "681d960886023b6d49274ab9ce6eeec59b9abcf968da83de81683be2125e1af2"
-  version "0.1.0"
+  url "https://github.com/TomoyukiSugiyama/dotfiles/releases/download/v0.2.0/dotfiles-0.2.0-universal-apple-darwin.tar.gz"
+  sha256 "c7c190cba4dfe692b7282594949c6e4418a80299551ef00a26d7538299f35154"
+  version "0.2.0"
   license "MIT"
 
   def install
